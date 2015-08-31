1a2b975ea95ae12540f7c78974158e050c55639f
André Willik Valenti <awvalenti@gmail.com>
2015-08-31 11:22:54 -0300

    Exporting libs correctly, at least on Windows

--- WiiUseJ/src/main/java/wiiusej/ClasspathResourceExporter.java
+++ WiiUseJ/src/main/java/wiiusej/ClasspathResourceExporter.java
@@ -9,11 +9,15 @@ import java.io.OutputStream;
 public class ClasspathResourceExporter {
 
 	public static String exportToTempFile(String resourcePath, String fileSuffix) {
+		String filePath = System.getProperty("java.io.tmpdir");
+		String fileName = resourcePath.substring(resourcePath.lastIndexOf('/') + 1);
+		File tempFile = new File(filePath + fileName);
+
 		InputStream input = null;
 		OutputStream output = null;
+
 		try {
 			input = ClasspathResourceExporter.class.getResourceAsStream(resourcePath);
-			File tempFile = File.createTempFile("wiimotej-lib", fileSuffix);
 			output = new FileOutputStream(tempFile);
 			transferStreams(input, output);
 			return tempFile.getAbsolutePath();
@@ -37,7 +41,7 @@ public class ClasspathResourceExporter {
 	}
 
 	private static void transferStreams(InputStream input, OutputStream output) throws IOException {
-		byte[] buffer = new byte[2048];
+		byte[] buffer = new byte[4096];
 		while (input.available() > 0) {
 			int size = input.read(buffer);
 			output.write(buffer, 0, size);
